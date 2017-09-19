340e5b3503b4befb8d4c5c063cb9e20e12c2791c
ezio84 <brabus84@gmail.com>
2017-09-18 20:54:20 -0400

    Add Turbo from Oreo
    Needed for: http://gerrit.dirtyunicorns.com/108
    
    Change-Id: I2e0fdac90642eb236604ff292b67d772b9dae3c8

--- config/common.mk
+++ config/common.mk
@@ -167,6 +167,10 @@ WITH_CM_CHARGER := false
 #ifeq ($(DEFAULT_ROOT_METHOD),rootless)
 #endif
 
+# Turbo
+PRODUCT_PACKAGES += \
+    Turbo
+
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
     libffmpeg_extractor \
