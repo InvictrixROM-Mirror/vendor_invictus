DesolationRebase/vendor_deso

Add Substratum Clean script [2/2]
We have an issue where Subs overlays can interfere and cause fc's after a clean flash. Let's just clean overlays when we flash

The script is pulled straight from subs rescue zip

common.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/deso/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
+# Substratum Clean Tool
+PRODUCT_COPY_FILES += \
+    vendor/deso/prebuilt/common/bin/substraumclean.sh:install/bin/substraumclean.sh \
+
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/bin/sysinit:system/bin/sysinit \

InvictrixROM-Mirror/vendor_invictus

config/common.mk
@@ -24,6 +24,11 @@ PRODUCT_COPY_FILES += \
     vendor/invictus/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/invictus/prebuilt/common/bin/50-invictus.sh:system/addon.d/50-invictus.sh
 
+# Substratum Clean Tool
+PRODUCT_COPY_FILES += \
+    vendor/invictus/prebuilt/common/bin/substraumclean.sh:install/bin/substraumclean.sh \
+it
+
 # Backup services whitelist
 PRODUCT_COPY_FILES += \
     vendor/invictus/config/permissions/backup.xml:system/etc/sysconfig/backup.xml